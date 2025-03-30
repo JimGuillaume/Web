74f3e8a3c69dc535018eb3b3dce1d7bc56c1e25b
Guillaume Jim <GuillaumeJim1999@gmail.com>
2025-03-30 16:08:47 +0200

    Prettier

--- js/carousel.js
+++ js/carousel.js
@@ -1,14 +1,12 @@
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(offset) {
-  
-  showSlides(slideIndex += offset);
+  showSlides((slideIndex += offset));
 }
 
 function currentSlide(current) {
-  showSlides(slideIndex = current);
+  showSlides((slideIndex = current));
 }
 
 function showSlides(SlideNumber) {
@@ -16,8 +14,14 @@ function showSlides(SlideNumber) {
   let slides = document.getElementsByClassName("mySlides");
 
   //Sécurité pour rester dans 0 < slideIndex < slides.length
-  if (SlideNumber > 4) {slideIndex = 1; SlideNumber = 1;}
-  if (SlideNumber < 1) {slideIndex = 4; SlideNumber = 4;}
+  if (SlideNumber > 4) {
+    slideIndex = 1;
+    SlideNumber = 1;
+  }
+  if (SlideNumber < 1) {
+    slideIndex = 4;
+    SlideNumber = 4;
+  }
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
