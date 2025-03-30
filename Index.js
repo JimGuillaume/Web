1dff16e7d907314601a9d57b4d3dd7cc47ad47a6
Guillaume Jim <GuillaumeJim1999@gmail.com>
2025-03-30 13:56:59 +0200

    Update on animaux.html Added last part & updated css & js to review it

--- Index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('dropDownMenu').addEventListener('click', function() {
-  var DropItem = document.getElementById('accueilItem');
-  if (DropItem.style.display === 'none' || DropItem.style.display === '') {
-      DropItem.style.display = 'flex';
-  } else {
-      DropItem.style.display = 'none';
-  }
-});
--- carousel.js
+++ carousel.js
@@ -14,23 +14,18 @@ function currentSlide(current) {
 function showSlides(SlideNumber) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
 
   //Sécurité pour rester dans 0 < slideIndex < slides.length
-  if (SlideNumber > slides.length) {slideIndex = 1}
-  if (SlideNumber < 1) {slideIndex = slides.length}
+  if (SlideNumber > 4) {slideIndex = 1; SlideNumber = 1;}
+  if (SlideNumber < 1) {slideIndex = 4; SlideNumber = 4;}
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
   }
 
   for (i = 0; i < slides.length; i++) {
     if (slides[i].dataset.slideNumber == slideIndex) {
       slides[i].style.display = "block";
-      dots[slideIndex-1].className += " active";
     }
   }
 }
